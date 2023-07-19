0c61006983fec11abf87d67fd022d99aa85f1ec5
nossbigg <chenggibson@gmail.com>
2023-07-19 02:02:09 +0100

    Make test work (windows)

--- jest.config.ts
+++ jest.config.ts
@@ -153,7 +153,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)'],
+  // testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
